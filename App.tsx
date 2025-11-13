87dbfd486238d25f2ca913683c911eb8346ee31b
Luis Ducasse <ducassecruz@gmail.com>
2025-11-13 12:08:17 -0400

    Add i18n support and update dependencies
    Integrated i18next and L10nProvider for localization in App.tsx. Renamed metro.config.js to metro.config.ts. Updated package.json: bumped version to 1.0.0, added i18n-related and date libraries, modified Expo scripts, and adjusted dependencies for improved compatibility.

--- App.tsx
+++ App.tsx
@@ -6,9 +6,12 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { MD3LightTheme, PaperProvider } from 'react-native-paper';
 import { StatusBar } from 'expo-status-bar';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './src/i18n';
 
 import persist from './src/config/store';
 import Main from './src/Main';
+import { L10nProvider } from './src/contexts/L10nContext';
 
 const { store, persistor } = persist();
 
@@ -22,19 +25,22 @@ const theme = {
 };
 
 const App: React.FC = () => (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-        <SafeAreaProvider>
-            <PaperProvider theme={theme}>
-                <ReduxProvider store={store}>
-                    <PersistGate loading={null} persistor={persistor}>
-                        <StatusBar style="dark" />
-                        <Main />
-                    </PersistGate>
-                </ReduxProvider>
-            </PaperProvider>
-        </SafeAreaProvider>
-    </GestureHandlerRootView>
+    <I18nextProvider i18n={i18n}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <SafeAreaProvider>
+                <PaperProvider theme={theme}>
+                    <ReduxProvider store={store}>
+                        <PersistGate loading={null} persistor={persistor}>
+                            <L10nProvider>
+                                <StatusBar style="dark" />
+                                <Main />
+                            </L10nProvider>
+                        </PersistGate>
+                    </ReduxProvider>
+                </PaperProvider>
+            </SafeAreaProvider>
+        </GestureHandlerRootView>
+    </I18nextProvider>
 );
 
 export default App;
-
